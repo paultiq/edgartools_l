28181f4c36d79f6d9e86794b6b12e60d8cde2c28
paul@iqmo.com <paul@iqmo.com>
2025-07-18 20:36:41 -0400

    add the postgres rate limiter, and option to use it for testing

--- edgar/httpclient_ratelimiter.py
+++ edgar/httpclient_ratelimiter.py
@@ -16,6 +16,22 @@ log = logging.getLogger(__name__)
 def create_rate_limiter(requests_per_second: int, max_delay: int) -> Limiter:
     return Limiter(Rate(requests_per_second, Duration.SECOND), raise_when_fail=False, max_delay=max_delay)
 
+
+def create_postgres_rate_limiter(requests_per_second: int, max_delay: int, postgres_url: str, table_name: str = "edgar_pyrate") -> Limiter:
+    from pyrate_limiter import PostgresBucket, Rate, PostgresClock, Limiter, Duration
+    from psycopg_pool import ConnectionPool
+
+    rate = Rate(requests_per_second, Duration.SECOND)
+    
+    connection_pool = ConnectionPool(postgres_url) # 'postgresql://postgres:pass@localhost:5432/ratelimit'
+
+    clock = PostgresClock(connection_pool)
+    rates = [rate]
+    bucket = PostgresBucket(connection_pool, table_name, rates)
+    limiter = Limiter(bucket, raise_when_fail=False, max_delay=max_delay, clock=clock)
+    
+    return limiter
+
 class RateLimitingTransport(httpx.HTTPTransport):
     def __init__(self, limiter: Limiter):
         super().__init__()

0c96b3b16326e71672197f817eb07d936550a3bf
paul@iqmo.com <paul@iqmo.com>
2025-08-15 07:43:09 -0400

    change httpx cache dir, due to serialization change

--- edgar/httpclient.py
+++ edgar/httpclient.py
@@ -1,22 +1,24 @@
-from httpxthrottlecache import HttpxThrottleCache
-from edgar.core import get_identity, strtobool
 import os
 from contextlib import asynccontextmanager, contextmanager
-import httpx
-from typing import AsyncGenerator, Optional, Generator
 from pathlib import Path
-from .core import edgar_data_dir
+from typing import AsyncGenerator, Generator, Optional
 
+import httpx
+from httpxthrottlecache import HttpxThrottleCache
+
+from edgar.core import get_identity, strtobool
+
+from .core import edgar_data_dir
 
 MAX_SUBMISSIONS_AGE_SECONDS = 10 * 60  # Check for submissions every 10 minutes
 MAX_INDEX_AGE_SECONDS = 30 * 60  # Check for updates to index (ie: daily-index) every 30 minutes
 
-# rules are regular expressions matching the request url path: 
+# rules are regular expressions matching the request url path:
 # The value determines whether it is cached or not:
 # - int > 0: how many seconds it'll be considered valid. During this time, the cached object will not be revalidated.
 # - False or 0: Do not cache
 # - True: Cache forever, never revalidate
-# - None: Determine cachability using response cache headers only. 
+# - None: Determine cachability using response cache headers only.
 #
 # Note that: revalidation consumes rate limit "hit", but will be served from cache if the data hasn't changed.
 
@@ -31,8 +33,9 @@ CACHE_RULES = {
     }
 }
 
+
 def get_cache_directory() -> str:
-    cachedir = Path(edgar_data_dir) / "_pcache"
+    cachedir = Path(edgar_data_dir) / "_tcache"
     cachedir.mkdir(parents=True, exist_ok=True)
 
     return str(cachedir)
@@ -58,8 +61,11 @@ def get_http_mgr(cache_enabled: bool = True, request_per_sec_limit: int = 9) -> 
         cache_mode = "Disabled"
 
     http_mgr = HttpxThrottleCache(
-        user_agent_factory=get_identity, cache_dir=cache_dir, cache_mode=cache_mode, request_per_sec_limit=request_per_sec_limit,
-        cache_rules = CACHE_RULES
+        user_agent_factory=get_identity,
+        cache_dir=cache_dir,
+        cache_mode=cache_mode,
+        request_per_sec_limit=request_per_sec_limit,
+        cache_rules=CACHE_RULES,
     )
     http_mgr.httpx_params["verify"] = get_edgar_verify_ssl
     return http_mgr
